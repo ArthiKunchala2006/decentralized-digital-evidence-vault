2e6f5b72080b1cab75db64253a81633e2bb71f09
ArthiKunchala2006 <arthikunchala28@gmail.com>
2026-07-23 15:10:02 +0530

    Add files via upload

---
+++ # Request input from the user.py
@@ -0,0 +1,9 @@
+# Request input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculate the sum
+total_sum = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is {total_sum}")
